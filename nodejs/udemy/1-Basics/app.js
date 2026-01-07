ee0beda0bb25e3f806e689b53ed8e7dcb82f988e
Oliver Zott <zott_oliver@web.de>
2026-01-07 17:38:08 +0100

    refactor: Improve async return logic in raw nodejs server example

--- nodejs/udemy/1-Basics/app.js
+++ nodejs/udemy/1-Basics/app.js
@@ -1,45 +1,8 @@
 // const http = require("http");
 import http, { request } from "http";
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === "/") {
-        res.write("Welcome to the homepage");
-        return res.end();
-    }
+import requestHandler from "./routes.js";
 
-    // Redirect example
-    if (url === "/test" && method === "GET") {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        res.write("You have reached the message endpoint");
-        return res.end();
-    }
-
-    // POST with body parsing example
-    if (url === "/data" && method === "POST") {
-        const body = [];
-
-        req.on("data", chunk => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        req.on("end", () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(`Parsed body: ${parsedBody}`);
-            res.write("You have reached the data endpoints end");
-        });
-        res.statusCode = 200;
-        res.write("Some data received");
-        return res.end();
-    }
-
-    console.log(req.url, req.method, req.headers);
-    res.setHeader("Content-Type", "text/html");
-    res.write("Hello World!");
-    res.end();
-});
+const server = http.createServer(requestHandler);
 
 server.listen(3000);
