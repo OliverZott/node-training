0cc28d5400ba2fa5b1cf723a683a319cb5d213f3
Oliver Zott <zott_oliver@web.de>
2026-01-12 20:07:30 +0100

    express - body parser

--- nodejs/udemy/2-Express/app.js
+++ nodejs/udemy/2-Express/app.js
@@ -2,14 +2,21 @@ import express from "express";
 
 const app = express();
 
-// Middleware that every request runs through
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log("In first middleware!");
+    next();
+});
+
+// Middleware that every request runs through
+app.post("/product", (req, res, next) => {
     console.log("Request:", req.method, req.url);
+    console.log(req.body);
     next(); // allows request to continue to next middleware!
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
     console.log("In second middleware!");
     res.status(200).json("Thats the response object :)");
 });
